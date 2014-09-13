73ce8db156d335d2e669052e37cd5084cb3e74d3
vrebierr <vrebierr@student.42.fr>
2014-09-13 23:15:24 +0200

    indent

--- client/app/admin/admin_event/admin_event.js
+++ client/app/admin/admin_event/admin_event.js
@@ -1,17 +1,17 @@
 'use strict';
 
 angular.module('integrationApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('admin_event', {
-        url: '/admin/event',
-        templateUrl: 'app/admin/admin_event/admin_event.html',
-        controller: 'AdminEventCtrl',
-        authenticate: true,
-        resolve: {
-    		events: function (Restangular) {
-    			Restangular.all('events').getList();
-        	}
-        }
-      });
-  });
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('admin_event', {
+                url: '/admin/event',
+                templateUrl: 'app/admin/admin_event/admin_event.html',
+                controller: 'AdminEventCtrl',
+                authenticate: true,
+                resolve: {
+            		events: function (Restangular) {
+            			Restangular.all('events').getList();
+                	}
+                }
+            });
+    });
