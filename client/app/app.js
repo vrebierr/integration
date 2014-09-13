9a14d286be84afa94b5c303b0b8ee8414930c76c
vrebierr <vrebierr@student.42.fr>
2014-09-13 16:08:34 +0200

    admin map + geoloc + event api

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('integrationApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'google-maps'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,5 +2,22 @@
 
 angular.module('integrationApp')
 	.controller('MainCtrl', function ($scope, socket) {
+		$scope.map = {
+			center: {
+				latitude: 48.89670230000001,
+				longitude: 2.3183781999999997
+			},
+			zoom: 12
+		};
 
+		$scope.markers = [
+			{coords: {latitude: 48.8967117, longitude: 2.3184350}},
+			{coords: {latitude: 48.8967120, longitude: 2.4184390}}
+		];
+
+		navigator.geolocation.watchPosition(function (pos) {
+			$scope.currentPosition = pos;
+			console.log(pos);
+			$scope.markers.push({coords: {latitude: pos.coords.latitude, longitude: pos.coords.longitude}})
+		});
 	});
